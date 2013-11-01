33d7c04c50349a879320adcab043b717dabcb229
Nick Stevens <nick@nickstevens.com>
2013-11-01 16:54:10 -0600

    Transferred everything into node/express/jade

--- server/app.js
+++ server/app.js
@@ -27,9 +27,7 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-// Create about.jade and get here
 app.get('/', routes.index);
-app.get('/about', routes.about);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,3 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
-
-exports.about = function(req, res){
-  res.render('about', { title: 'Express' });
-};
