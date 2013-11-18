23bb095a8b9bc3ce16833d3d4b8f7160b7d2c7c8
Nick Stevens <nick@nickstevens.com>
2013-11-18 12:03:01 -0700

    Added new jade template and route for product-list.

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/product-detail', routes.productDetail);
+app.get('/product-list', routes.productList);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,13 @@ exports.index = function(req, res){
 };
 
 exports.about = function(req, res){
-  res.render('about', { title: 'Express' });
+  res.render('about', { title: 'About Messina' });
 };
 
 exports.productDetail = function(req, res){
-  res.render('product-detail', { title: 'Express' });
+  res.render('product-detail', { title: 'Messina: Product' });
+};
+
+exports.productList = function(req, res){
+  res.render('product-list', { title: 'Messina: Product List' });
 };
